ea01ddad1663d722bb3afdb1e2ff1df9793d9b23
Haakon36 <aareskjold.haakon@gmail.com>
2019-11-25 16:39:57 +0100

    added file for forms with validations in php

---
+++ experimental/forms.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="send.php" method="post">
+        First Name: <input type="text" name="fname"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+        </form>
+        
+    </body>
+</html>
---
+++ experimental/send.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        
+    First Name: <?php echo $_POST['fname']; ?><br>
+    Email: <?php echo $_POST['email']; ?><br>
+    
+    </body>
+</html>
